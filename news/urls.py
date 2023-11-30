6a4a107816ec6e7c370f57678ff1cc88855d07b7
jeferson-viana <jefersonv28@gmail.com>
2023-11-30 00:51:25 -0300

    FEAT: Criando o template news_details.html - (Requisito 5)

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, news_detail
 
 urlpatterns = [
     path('', homepage, name='home-page'),
+    path('news/<int:id>', news_detail, name='news-details-page'),
 ]
--- news/views.py
+++ news/views.py
@@ -5,3 +5,8 @@ from .models import News
 def homepage(request):
     context = {"news": News.objects.all(), "title": "Trybe News"}
     return render(request, 'home.html', context)
+
+
+def news_detail(request, id):
+    context = {"new": News.objects.get(id=id)}
+    return render(request, 'news_details.html', context)
