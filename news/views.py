e24018a70850cd84a6681c1098918ec27d39d19e
jeferson-viana <jefersonv28@gmail.com>
2023-11-30 00:12:14 -0300

    FEAT: Criando o template home.html - (Requisito 4)

--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+
+def homepage(request):
+    context = {"news": News.objects.all(), "title": "Trybe News"}
+    return render(request, 'home.html', context)
